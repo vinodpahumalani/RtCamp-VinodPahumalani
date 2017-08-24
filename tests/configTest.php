dea9cf2b73b2ff5e08fb94ac2a07b4ef3dddc735
vinodpahumalani <30702606+vinodpahumalani@users.noreply.github.com>
2017-08-24 23:45:37 +0530

    Update configTest.php

--- tests/configTest.php
+++ tests/configTest.php
@@ -1,19 +1,21 @@
 <?php
 
-	static $token=require_once(__DIR__.'/../src_code/config.php');
+	
 
 class configTest extends PHPUnit_Framework_TestCase
 {
    
-
-    
+	protected $config;
+    	protected $token;
    
     public function testTokens()
     {
-       
-        $this->assertEquals('', $token['consumer_key']);
-		$this->assertEquals('', $token['consumer_secret']);
-		$this->assertEquals('https://vinodpahumalani.000webhostapp.com/index.php', $token['url_login']);
-		$this->assertEquals('https://vinodpahumalani.000webhostapp.com/home.php', $token['url_home']);
+       $this->config=new vinodpahumalani/RtCamp-VinodPahumalani/config();
+	 $this->token=$this->config->token();   
+	    
+        $this->assertEquals('', $this->token['consumer_key']);
+		$this->assertEquals('', $this->token['consumer_secret']);
+		$this->assertEquals('https://vinodpahumalani.000webhostapp.com/index.php', $this->token['url_login']);
+		$this->assertEquals('https://vinodpahumalani.000webhostapp.com/home.php', $this->token['url_home']);
     }
 }

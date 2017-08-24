ca9b08f674130082c51f1187ee829a86ff763f9f
vinodpahumalani <30702606+vinodpahumalani@users.noreply.github.com>
2017-08-25 00:09:32 +0530

    Update configTest.php

--- tests/configTest.php
+++ tests/configTest.php
@@ -10,7 +10,7 @@ class configTest extends PHPUnit_Framework_TestCase
    
     public function testTokens()
     {
-       $this->config=new \vinodpahumalani\RtCamp\config();
+       $this->config=new config();
 	 $this->token=$this->config->token();   
 	    
         $this->assertEquals('', $this->token['consumer_key']);
